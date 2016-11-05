c96dc26e044f7fbd7e510dab68e3de565507b6b5
Roxasch <kztoth@umass.edu>
2016-11-05 04:03:33 -0400

    pushing eddited add files

--- js/add.js
+++ js/add.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
-    $(document.getElementById('add_trade')).click(
+    $(document.getElementById('add_trade_give')).click(
         function(){
-        console.log("work");
-        $('<ul class="media box"><div class="media-body">{{ item_name }}</div><div class="media-right"><a>X</a></div></ul>').insertBefore($(this)).text("");
-        return false;
+        	var addedItem = $(document.getElementById('give_new_item')).val();
+        	if(addedItem != ''){
+        		console.log(addedItem);
+        		$(document.getElementById('give_container')).append('<ul class="media box"><div class="media-body">'+addedItem+'</div><div class="media-right"><a onclick="$(this).closest(\'ul\').remove();">X</a></div></ul>')
+        		$(document.getElementById('give_new_item')).val('');
+        	}
+        	return false;
+    });
+
+    $(document.getElementById('add_trade_get')).click(
+        function(){
+        	var addedItem = $(document.getElementById('request_new_item')).val();
+        	if(addedItem != ''){
+        		console.log(addedItem);
+        		$(document.getElementById('get_container')).append('<ul class="media box"><div class="media-body">'+addedItem+'</div><div class="media-right"><a onclick="$(this).closest(\'ul\').remove();">X</a></div></ul>')
+        		$(document.getElementById('request_new_item')).val('');
+        	}
+        	return false;
     });
 })
